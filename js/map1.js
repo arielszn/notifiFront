197c0bf2fceeb084ed8adb9634597a5cf22d8875
Ben Krawitz <benkraw@umich.edu>
2014-06-25 12:59:13 +0300

    everything

---
+++ js/map1.js
@@ -0,0 +1,190 @@
+var map, heatmap;
+var markers =[];
+var bird = 'images/bird.png';
+var position;
+var topic = 'jews';
+
+
+function initialize() {
+
+  position = new google.maps.LatLng(32.0700131,34.794036);
+
+  var mapOptions = {
+    center: position,
+    zoom: 15,
+    mapTypeId: google.maps.MapTypeId.MAP,
+    disableDefaultUI: true,
+    zoomControl: false,
+    disableDoubleClickZoom: true,
+    scrollwheel: false,
+    scaleControl: true,
+    navigationControl: true,
+    draggable: true
+  };
+
+  map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
+
+  heatmap = new google.maps.visualization.HeatmapLayer({
+  });
+
+  heatmap.setMap(map);
+
+  google.maps.event.addDomListener(window, "resize", function() { //RESIZE
+   var center = map.getCenter();
+   google.maps.event.trigger(map, "resize");
+   map.setCenter(center); 
+  });
+
+  google.maps.event.addListener(map, 'dragend', function() { position = map.getCenter();
+                                                             drawFakeDataAround(); 
+                                                           } );
+
+  drawFakeDataAround(position);
+
+}
+
+function setTopic(newTopic){
+  topic = newTopic;
+  drawFakeDataAround();
+}
+
+function drawFakeDataAround(){
+  
+  var multiplierLat = 2;
+  var multiplierLong = 2;
+  if(topic=='protest'){
+    multiplierLat = 0.3;
+    multiplierLong=0.2;
+  }
+
+  var fakeData = []
+
+  for(i=-25;i<25;i++){
+    fakeData.push({
+                    "latitude": position.lat()+i*0.0005*Math.random()*multiplierLat,
+                    "longitude": position.lng()+i*0.001*Math.random()*multiplierLong,
+                    "tweet": topic+" fake data "+i,
+                    "retweetsCount": Math.abs(i), 
+                    "favouritesCount": Math.abs(i),
+                    "user": "user_"+i 
+                  });
+  }
+
+  paintMap(fakeData);
+
+}
+
+function protestaSimulation(tweetsData){
+
+  alert('heyy');
+
+  var mapData = [];
+  var markerContent = '';
+
+  tweetsData.forEach(function(tweet){
+    var point = new google.maps.LatLng(tweet.latitude, tweet.longitude);
+    mapData.push(point);
+
+    markerContent +=  '<div id="content">'+
+                        '<div id="siteNotice">'+
+                        '</div>'+
+                        '<h1 id="firstHeading" class="firstHeading">'+
+                          '<a target=\'blank\' href="https://twitter.com/arielszn">@'+tweet.user+'</a>' +
+                        '</h1>'+
+                        '<div id="bodyContent">'+
+                          '<p><b>'+tweet.tweet+'</b></p>'+
+                          '<p>Retweets: ' + tweet.retweetsCount + '</p>' +
+                          '<p>Favs: ' + tweet.favouritesCount + '</p>' +
+                        '</div>'+
+                      '</div> <br/> <hr> <br/>';
+
+    
+
+  }); 
+
+  var marker = new google.maps.Marker({
+    position: position,
+    map: map,
+    title:  "Protest",
+    icon: bird
+  });
+
+  var infowindow = new google.maps.InfoWindow({
+    content: markerContent,
+    maxWidth: 200
+  });
+
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.open(map,marker);
+  });
+
+  markers.push(marker);
+
+  var pointArray = new google.maps.MVCArray(mapData);
+
+  heatmap.set('data', pointArray);
+}
+
+function paintMap(tweetsData) {
+
+  var mapData = [];
+  cleanMarkers();
+
+  if(topic=='protest'){
+    protestaSimulation(tweetsData);
+    return;
+  }
+
+  tweetsData.forEach(function(tweet){
+    var point = new google.maps.LatLng(tweet.latitude, tweet.longitude);
+    mapData.push(point);
+
+    var marker = new google.maps.Marker({
+      position: point,
+      map: map,
+      title:  "\"" + tweet.tweet + "\"\r ",
+      icon: bird
+    });
+
+    var contentString =  '<div id="content">'+
+                            '<div id="siteNotice">'+
+                            '</div>'+
+                            '<h1 id="firstHeading" class="firstHeading">'+
+                              '<a target=\'blank\' href="https://twitter.com/arielszn">@'+tweet.user+'</a>' +
+                            '</h1>'+
+                            '<div id="bodyContent">'+
+                              '<p><b>'+tweet.tweet+'</b></p>'+
+                              '<p>Retweets: ' + tweet.retweetsCount + '</p>' +
+                              '<p>Favs: ' + tweet.favouritesCount + '</p>' +
+                            '</div>'+
+                          '</div>';
+
+    var infowindow = new google.maps.InfoWindow({
+        content: contentString,
+        maxWidth: 150
+    });
+
+    google.maps.event.addListener(marker, 'click', function() {
+      infowindow.open(map,marker);
+    });
+
+    markers.push(marker);
+
+  }); 
+
+  var pointArray = new google.maps.MVCArray(mapData);
+
+  heatmap.set('data', pointArray);
+}
+
+function cleanMarkers(){
+  markers.forEach(function(marker){
+    marker.setMap(null);
+  })
+
+  markers = [];
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
